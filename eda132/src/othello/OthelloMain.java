f193793f07fe7b8e86e67d249dedf86574fd26ce
ShanSenanayake <senanayake92@gmail.com>
2015-01-28 14:58:21 +0100

    doing stuff

--- eda132/src/othello/OthelloMain.java
+++ eda132/src/othello/OthelloMain.java
@@ -3,12 +3,21 @@ package othello;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 
 public class OthelloMain {
 	private static int EMPTY = 0;
 	private static int LIGHT = 1;
 	private static int DARK = 2;
-
+	private static final int N = 1;
+	private static final int S = 2;
+	private static final int W = 3;
+	private static final int E = 4;
+	private static final int NW = 5;
+	private static final int NE = 6;
+	private static final int SW = 7;
+	private static final int SE = 8;
+	
 	public static void main(String args[]) {
 		int[][] board = new int[8][8];
 		board[4][3] = 1;
@@ -38,7 +47,7 @@ public class OthelloMain {
 
 	public static void printBoard(int[][] board) {
 		for (int i1 = 0; i1 < board.length; i1++) {
-			System.out.print("   " + (i1 + 1));
+			System.out.print("    " + (i1 + 1));
 		}
 		System.out.print("\n  +");
 		for (int i = 0; i < board.length; i++) {
@@ -70,6 +79,11 @@ public class OthelloMain {
 		}
 
 	}
+	
+	public static String posToString(int x,int y){
+		char[] c = {(char)(x+'a'), (char)(y+'1')};
+		return new String(c);
+	}
 
 	public static void AI() {
 
@@ -82,8 +96,85 @@ public class OthelloMain {
 
 	}
 
-	public static String[] findValidMoves(int[][] board) {
+	public static String[] findValidMoves(int[][] board, int player) {
+		for(int i = 0; i<board.length;i++){
+			for(int j = 0; j<board.length;j++){
+				if(board[i][j] == EMPTY){
+					
+				}
+			}
+		}
 		return null;
 	}
-
+	public static HashSet<Integer[]> checkDirection(int[][] board,int player, int x, int y, int dir){
+		HashSet<Integer[]> score = new HashSet<Integer[]>();
+		switch(dir){
+		case N: 
+			for(int i = (x-1); i>=0; i--){
+				if(board[i][y] == (player % 2 +1)){ 
+					Integer[] temp = new Integer[2];
+					temp[0] = i;
+					temp[1] = y;
+					score.add(temp);
+				}else if(board[i][y] == 0){
+					return new HashSet<Integer[]>();
+				}else{
+					return score;
+				}
+			}
+			break;
+		case S:
+			for(int i = (x+1); i<board.length; i++){
+				if(board[i][y] == (player % 2 +1)){ 
+					Integer[] temp = new Integer[2];
+					temp[0] = i;
+					temp[1] = y;
+					score.add(temp);
+				}else if(board[i][y] == 0){
+					return new HashSet<Integer[]>();
+				}else{
+					return score;
+				}
+			}
+			break;
+		case E:
+			for(int i = (y+1); i<board.length; i++){
+				if(board[x][i] == (player % 2 +1)){ 
+					Integer[] temp = new Integer[2];
+					temp[0] = x;
+					temp[1] = i;
+					score.add(temp);
+				}else if(board[x][i] == 0){
+					return new HashSet<Integer[]>();
+				}else{
+					return score;
+				}
+			}
+			break;
+		case W:
+			for(int i = (y-1); i>=0; i--){
+				if(board[x][i] == (player % 2 +1)){ 
+					Integer[] temp = new Integer[2];
+					temp[0] = x;
+					temp[1] = i;
+					score.add(temp);
+				}else if(board[x][i] == 0){
+					return new HashSet<Integer[]>();
+				}else{
+					return score;
+				}
+			}
+			break;
+		case NW:
+			break;
+		case NE:
+			break;
+		case SW:
+			break;
+		case SE:
+			break;
+			
+		}
+		return score;
+	}
 }

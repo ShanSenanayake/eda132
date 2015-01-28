98cb71cd9f80235e924a19cae48e7881291f82c6
ShanSenanayake <senanayake92@gmail.com>
2015-01-28 16:09:39 +0100

    Spelbart reversi om du inte gör 'invalid moves'!

--- eda132/src/othello/OthelloMain.java
+++ eda132/src/othello/OthelloMain.java
@@ -3,21 +3,14 @@ package othello;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class OthelloMain {
 	private static int EMPTY = 0;
 	private static int LIGHT = 1;
 	private static int DARK = 2;
-	private static final int N = 1;
-	private static final int S = 2;
-	private static final int W = 3;
-	private static final int E = 4;
-	private static final int NW = 5;
-	private static final int NE = 6;
-	private static final int SW = 7;
-	private static final int SE = 8;
-	
+
 	public static void main(String args[]) {
 		int[][] board = new int[8][8];
 		board[4][3] = 1;
@@ -34,7 +27,7 @@ public class OthelloMain {
 			for (int i = 0; i < 60; i++) {
 				System.out.print("\nPlayer " + player + " make your move:");
 				s = br.readLine();
-				board = makeMove(board,s,player);
+				board = makeMove(board, s, player);
 				player = player % 2 + 1;
 				printBoard(board);
 			}
@@ -43,7 +36,6 @@ public class OthelloMain {
 			e.printStackTrace();
 		}
 	}
-
 
 	public static void printBoard(int[][] board) {
 		for (int i1 = 0; i1 < board.length; i1++) {
@@ -79,9 +71,9 @@ public class OthelloMain {
 		}
 
 	}
-	
-	public static String posToString(int x,int y){
-		char[] c = {(char)(x+'a'), (char)(y+'1')};
+
+	public static String posToString(int x, int y) {
+		char[] c = { (char) (x + 'a'), (char) (y + '1') };
 		return new String(c);
 	}
 
@@ -89,92 +81,80 @@ public class OthelloMain {
 
 	}
 
-	public static int[][] makeMove(int[][] board, String pos, int y) {
-		char[] c = pos.toCharArray();
-		board[(int)(c[0]-'a')][(int)(c[1]-'1')] = y;
+	public static int[][] makeMove(int[][] board, String pos, int player) {
+		HashMap<String, HashSet<Integer[]>> moves = findValidMoves(board,
+				player);
+		if (moves.containsKey(pos)) {
+			flipTiles(board, moves.get(pos), player);
+			char[] c = pos.toCharArray();
+			board[(int) (c[0] - 'a')][(int) (c[1] - '1')] = player;
+		}
 		return board;
 
 	}
 
-	public static String[] findValidMoves(int[][] board, int player) {
-		for(int i = 0; i<board.length;i++){
-			for(int j = 0; j<board.length;j++){
-				if(board[i][j] == EMPTY){
-					
-				}
-			}
+	public static int[][] flipTiles(int[][] board,
+			HashSet<Integer[]> toBeFlipped, int player) {
+		for (Integer[] i : toBeFlipped) {
+			board[i[0]][i[1]] = player;
 		}
-		return null;
+		return board;
 	}
-	public static HashSet<Integer[]> checkDirection(int[][] board,int player, int x, int y, int dir){
-		HashSet<Integer[]> score = new HashSet<Integer[]>();
-		switch(dir){
-		case N: 
-			for(int i = (x-1); i>=0; i--){
-				if(board[i][y] == (player % 2 +1)){ 
-					Integer[] temp = new Integer[2];
-					temp[0] = i;
-					temp[1] = y;
-					score.add(temp);
-				}else if(board[i][y] == 0){
-					return new HashSet<Integer[]>();
-				}else{
-					return score;
+
+	public static HashMap<String, HashSet<Integer[]>> findValidMoves(
+			int[][] board, int player) {
+		HashMap<String, HashSet<Integer[]>> moves = new HashMap<String, HashSet<Integer[]>>();
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				if (board[i][j] == EMPTY) {
+					// Check South Direction
+					HashSet<Integer[]> score = checkDirection(board, player, i,
+							j, 1, 0);
+					// Check North Direction
+					score.addAll(checkDirection(board, player, i, j, -1, 0));
+					// Check East Direction
+					score.addAll(checkDirection(board, player, i, j, -0, 1));
+					// Check West Direction
+					score.addAll(checkDirection(board, player, i, j, 0, -1));
+					// Check South-East Direction
+					score.addAll(checkDirection(board, player, i, j, 1, 1));
+					// Check South-West Direction
+					score.addAll(checkDirection(board, player, i, j, 1, -1));
+					// Check North-East Direction
+					score.addAll(checkDirection(board, player, i, j, -1, 1));
+					// Check North-West Direction
+					score.addAll(checkDirection(board, player, i, j, -1, -1));
+					if (!score.isEmpty()) {
+						moves.put(posToString(i, j), score);
+					}
 				}
 			}
-			break;
-		case S:
-			for(int i = (x+1); i<board.length; i++){
-				if(board[i][y] == (player % 2 +1)){ 
-					Integer[] temp = new Integer[2];
-					temp[0] = i;
-					temp[1] = y;
-					score.add(temp);
-				}else if(board[i][y] == 0){
-					return new HashSet<Integer[]>();
-				}else{
-					return score;
-				}
-			}
-			break;
-		case E:
-			for(int i = (y+1); i<board.length; i++){
-				if(board[x][i] == (player % 2 +1)){ 
-					Integer[] temp = new Integer[2];
-					temp[0] = x;
-					temp[1] = i;
-					score.add(temp);
-				}else if(board[x][i] == 0){
-					return new HashSet<Integer[]>();
-				}else{
-					return score;
-				}
-			}
-			break;
-		case W:
-			for(int i = (y-1); i>=0; i--){
-				if(board[x][i] == (player % 2 +1)){ 
-					Integer[] temp = new Integer[2];
-					temp[0] = x;
-					temp[1] = i;
-					score.add(temp);
-				}else if(board[x][i] == 0){
-					return new HashSet<Integer[]>();
-				}else{
-					return score;
-				}
-			}
-			break;
-		case NW:
-			break;
-		case NE:
-			break;
-		case SW:
-			break;
-		case SE:
-			break;
-			
 		}
-		return score;
+		return moves;
+	}
+
+	public static HashSet<Integer[]> checkDirection(int[][] board, int player,
+			int x, int y, int incX, int incY) {
+		HashSet<Integer[]> score = new HashSet<Integer[]>();
+		boolean undone = true;
+		while (undone) {
+			x = x + incX;
+			y = y + incY;
+			if (x < 0 || y < 0 || x >= board.length || y >= board.length) {
+				undone = false;
+			} else {
+				if (board[x][y] == (player % 2 + 1)) {
+					Integer[] temp = new Integer[2];
+					temp[0] = x;
+					temp[1] = y;
+					score.add(temp);
+				} else if (board[x][y] == 0) {
+					return new HashSet<Integer[]>();
+				} else {
+					return score;
+				}
+			}
+		}
+		return new HashSet<Integer[]>();
 	}
 }

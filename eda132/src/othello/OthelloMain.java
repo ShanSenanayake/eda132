a9e28503280746830d7ba028807a58ddeb74474e
ShanSenanayake <senanayake92@gmail.com>
2015-01-28 12:50:24 +0100

    stub methods

---
+++ eda132/src/othello/OthelloMain.java
@@ -0,0 +1,24 @@
+package othello;
+
+public class OthelloMain {
+	
+	
+	public static void main(String args[]){
+		
+	}
+	
+	public static int[][] printBoard(int[][] board){
+		return board;
+		
+	}
+	
+	public static void AI(){
+		
+	}
+	
+	public static int[][] makeMove(int[][] board, int x, int y){
+		return board;
+		
+	}
+
+}

d3ea6df2c0bd671d64294a28f412840514c06e0f
ShanSenanayake <senanayake92@gmail.com>
2015-01-29 12:19:28 +0100

    Lets play otheollo

--- eda132/src/othello/OthelloGame.java
+++ eda132/src/othello/OthelloGame.java
@@ -6,18 +6,21 @@ import java.util.HashSet;
 public class OthelloGame {
 	private static final int EMPTY = 0;
 	private static final int LIGHT = 1;
-	private static final int DARK = 2;
+	private static final int DARK = -1;
+	private static final char L = 'O';
+	private static final char D = 'X';
+	private static final char E = ' ';
+	private static final char V = '#';
 	private int[][] board;
 	private int player;
 	private HashMap<String, HashSet<Integer[]>> validMoves;
-	
+
 	public OthelloGame(int size) {
 		board = new int[size][size];
 		board[size / 2][size / 2 - 1] = LIGHT;
 		board[size / 2][size / 2] = DARK;
 		board[size / 2 - 1][size / 2 - 1] = DARK;
 		board[size / 2 - 1][size / 2] = LIGHT;
-		nextTurn();
 	}
 
 	public void print() {
@@ -35,11 +38,11 @@ public class OthelloGame {
 			for (int j = 0; j < board.length; j++) {
 				char ch;
 				if (board[i][j] == LIGHT) {
-					ch = 'O';
+					ch = L;
 				} else if (board[i][j] == DARK) {
-					ch = 'X';
+					ch = D;
 				} else {
-					ch = ' ';
+					ch = E;
 				}
 				System.out.print(" " + ch + " |");
 			}
@@ -62,8 +65,7 @@ public class OthelloGame {
 			for (int j = 0; j < board.length; j++) {
 				if (board[i][j] == EMPTY) {
 					// Check South Direction
-					HashSet<Integer[]> score = checkDirection(i, j, 1,
-							0);
+					HashSet<Integer[]> score = checkDirection(i, j, 1, 0);
 					// Check North Direction
 					score.addAll(checkDirection(i, j, -1, 0));
 					// Check East Direction
@@ -86,8 +88,7 @@ public class OthelloGame {
 		}
 	}
 
-	private HashSet<Integer[]> checkDirection(int x, int y,
-			int incX, int incY) {
+	private HashSet<Integer[]> checkDirection(int x, int y, int incX, int incY) {
 		HashSet<Integer[]> score = new HashSet<Integer[]>();
 		boolean undone = true;
 		while (undone) {
@@ -96,7 +97,7 @@ public class OthelloGame {
 			if (x < 0 || y < 0 || x >= board.length || y >= board.length) {
 				undone = false;
 			} else {
-				if (board[x][y] == (player % 2 + 1)) {
+				if (board[x][y] == opponent()) {
 					Integer[] temp = new Integer[2];
 					temp[0] = x;
 					temp[1] = y;
@@ -111,49 +112,87 @@ public class OthelloGame {
 		return new HashSet<Integer[]>();
 	}
 
+	private int opponent() {
+		if (player == LIGHT) {
+			return DARK;
+		} else {
+			return LIGHT;
+		}
+	}
+
 	public boolean makeMove(String pos) {
-		if (validMoves.containsKey(pos)){
+		if (validMoves.containsKey(pos)) {
 			flipTiles(validMoves.get(pos));
 			int[] p = stringToPos(pos);
 			board[p[0]][p[1]] = player;
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
-	
+
 	private void flipTiles(HashSet<Integer[]> toBeFlipped) {
 		for (Integer[] i : toBeFlipped) {
 			board[i[0]][i[1]] = player;
 		}
 	}
-	
-	public boolean nextTurn(){
+
+	public boolean nextTurn() {
 		switchPlayer();
 		findValidMoves();
 		return !validMoves.isEmpty();
 	}
-	
-	private void switchPlayer(){
-		if (player == DARK){
+
+	private void switchPlayer() {
+		if (player == DARK) {
 			player = LIGHT;
-		}else{
+		} else {
 			player = DARK;
 		}
 	}
-	
-	public HashMap<String, HashSet<Integer[]>> getValidMoves(){
-		return validMoves;		
+
+	public char getPlayer() {
+		if (player == LIGHT) {
+			return L;
+		} else {
+			return D;
+		}
 	}
-	
+
+	public HashMap<String, HashSet<Integer[]>> getValidMoves() {
+		return validMoves;
+	}
+
 	private String posToString(int x, int y) {
 		char[] c = { (char) (x + 'a'), (char) (y + '1') };
 		return new String(c);
 	}
-	
-	private int[] stringToPos(String pos){
+
+	private int[] stringToPos(String pos) {
 		char[] c = pos.toCharArray();
-		int[] result = {(int) (c[0] - 'a'),(int) (c[1] - '1')};
+		int[] result = { (int) (c[0] - 'a'), (int) (c[1] - '1') };
 		return result;
+	}
+
+	public String score() {
+		int score = sum();
+		if (score < 0) {
+			score = -score;
+			return "Player: " + D + " Score: " + score;
+		} else if (score > 0) {
+			return "Player: " + L + " Score: " + score;
+		} else {
+			return "Draw";
+		}
+	}
+
+	private int sum() {
+		int sum = 0;
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				sum += board[i][j];
+			}
+		}
+		return sum;
 	}
 }
--- eda132/src/othello/TestMain.java
+++ eda132/src/othello/TestMain.java
@@ -1,10 +1,32 @@
 package othello;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestMain {
 
 	public static void main(String[] args) {
 		OthelloGame ob = new OthelloGame(8);
-		ob.print();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Welcome to the classical game of Othello");
+		while(ob.nextTurn()){
+			ob.print();
+			char player = ob.getPlayer();
+			System.out.print("\nPlayer " + player + " make your move:");
+			try {
+				String s = br.readLine();
+				while(!ob.makeMove(s)){
+					System.out.print("\nInvalid move try again:");
+					s = br.readLine();
+				};
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Leading: " + ob.score());
+		}
+		System.out.println("Game over! Winner: " + ob.score());
 
 	}
 

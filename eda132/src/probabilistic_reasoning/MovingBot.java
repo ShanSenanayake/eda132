9cd61a62aba75f5423a580fc86f54b2b93937f66
ShanSenanayake <senanayake92@gmail.com>
2015-02-19 10:31:27 +0100

    moving the moving bot

--- eda132/src/probabilistic_reasoning/MovingBot.java
+++ eda132/src/probabilistic_reasoning/MovingBot.java
@@ -1,5 +1,17 @@
 package probabilistic_reasoning;
 
-public class MovingBot {
+import java.awt.Point;
+import java.util.Random;
 
+public class MovingBot {
+	private int sizeOfBoard;
+	private Point start;
+	private Random rnd;
+	
+	public MovingBot(int sizeOfBoard){
+		this.sizeOfBoard = sizeOfBoard;
+		rnd = new Random();
+		start = new Point(rnd.nextInt(sizeOfBoard),rnd.nextInt(sizeOfBoard));
+		
+	}
 }

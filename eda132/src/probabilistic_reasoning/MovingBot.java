fea6481eed3327da4f3983c2e96867cbac332131
ShanSenanayake <senanayake92@gmail.com>
2015-02-19 12:41:39 +0100

    stuff

--- eda132/src/probabilistic_reasoning/MovingBot.java
+++ eda132/src/probabilistic_reasoning/MovingBot.java
@@ -1,6 +1,6 @@
 package probabilistic_reasoning;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 
 public class MovingBot {
@@ -9,58 +9,90 @@ public class MovingBot {
 	private Random rnd;
 	private Point currentHeading;
 	private Point[] headings;
-	
-	public MovingBot(int sizeOfBoard){
+
+	public MovingBot(int sizeOfBoard) {
 		this.sizeOfBoard = sizeOfBoard;
 		rnd = new Random();
-		position = new Point(rnd.nextInt(sizeOfBoard),rnd.nextInt(sizeOfBoard));
-		headings = new Point[] {new Point(0,1), new Point(0,-1),new Point(1,0), new Point(-1,0)};
-		
-		
+		position = new Point(rnd.nextInt(sizeOfBoard), rnd.nextInt(sizeOfBoard));
+		headings = new Point[] { new Point(0, 1), new Point(0, -1),
+				new Point(1, 0), new Point(-1, 0) };
+		currentHeading = newRandomHeading();
+
 	}
-	
-	private Point newRandomHeading(){
+
+	private Point newRandomHeading() {
 		int index = rnd.nextInt(4);
-		if(currentHeading == headings[index]){
+		if (currentHeading == headings[index]) {
 			return newRandomHeading();
-		}else{
-			return newRandomHeading();
+		} else {
+			return headings[index];
+		}
+
+	}
+
+	private Point newRandomHeading(HashSet<Point> illegalHeadings) {
+		int index = rnd.nextInt(4);
+		if (illegalHeadings.contains(headings[index])) {
+			return newRandomHeading(illegalHeadings);
+		} else {
+			return headings[index];
+		}
+
+	}
+
+	public void move() {
+		double prob = rnd.nextDouble();
+		if ((position.x >= sizeOfBoard - 1 && position.y >= sizeOfBoard - 1)
+				|| (position.x <= 0 && position.y <= 0)) {
+			HashSet<Point> illegalHeadings = new HashSet<Point>();
+			if (position.x > 0) {
+				illegalHeadings.add(new Point(1, 0));
+				illegalHeadings.add(new Point(0, 1));
+			} else {
+				illegalHeadings.add(new Point(-1, 0));
+				illegalHeadings.add(new Point(0, -1));
+
+			}
+			currentHeading = newRandomHeading(illegalHeadings);
+
+		} else if (prob >= 0.7 || (position.x >= sizeOfBoard - 1)
+				|| (position.x <= 0) || (position.y >= sizeOfBoard - 1)
+				|| (position.y <= 0)) {
+			currentHeading = newRandomHeading();
 		}
 		
 	}
-	
-	public void move(){
-		
-	}
 
-
-	public Point sensorOutput(){
+	public Point sensorOutput() {
 		double prob = rnd.nextDouble();
-		if (prob < 0.1){
-			return new Point(position.x,position.y);
-		}else if (prob < 0.5){
+		if (prob < 0.1) {
+			return new Point(position.x, position.y);
+		} else if (prob < 0.5) {
 			int tmpX;
 			int tmpY;
 			Point p;
 			do {
 				tmpX = rnd.nextInt(3) - 1;
 				tmpY = rnd.nextInt(3) - 1;
-				p = new Point(position.x+tmpX, position.y+tmpY);
-			} while(tmpX == 0 && tmpY == 0 || p.x >= 0 && p.y >= 0 && p.x < sizeOfBoard && p.y < sizeOfBoard);
+				p = new Point(position.x + tmpX, position.y + tmpY);
+			} while (tmpX == 0 && tmpY == 0 || p.x >= 0 && p.y >= 0
+					&& p.x < sizeOfBoard && p.y < sizeOfBoard);
 			return p;
-		}else if (prob < 0.9){
+		} else if (prob < 0.9) {
 			int tmpX;
 			int tmpY;
 			Point p;
 			do {
-				tmpX = (rnd.nextInt(3) - 1)*2;
-				tmpY = (rnd.nextInt(3) - 1)*2;
-				p = new Point(position.x+tmpX, position.y+tmpY);
-			} while(tmpX == 0 && tmpY == 0 || Math.abs(tmpX) == 1 && Math.abs(tmpY) == 1 || p.x >= 0 && p.y >= 0 && p.x < sizeOfBoard && p.y < sizeOfBoard);
+				tmpX = (rnd.nextInt(3) - 1) * 2;
+				tmpY = (rnd.nextInt(3) - 1) * 2;
+				p = new Point(position.x + tmpX, position.y + tmpY);
+			} while (tmpX == 0 && tmpY == 0 || Math.abs(tmpX) == 1
+					&& Math.abs(tmpY) == 1 || p.x >= 0 && p.y >= 0
+					&& p.x < sizeOfBoard && p.y < sizeOfBoard);
 			return p;
-		}else{
-			return new Point(-1,-1);
+		} else {
+			return new Point(-1, -1);
 		}
 	}
-	
+
 }

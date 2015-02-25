24d14b03089dabdea2ba4d0f5101bd74cfcdb219
ShanSenanayake <senanayake92@gmail.com>
2015-02-25 15:08:43 +0100

    done some transistion stuff

--- eda132/src/probabilistic_reasoning/ForwardAlgorithm.java
+++ eda132/src/probabilistic_reasoning/ForwardAlgorithm.java
@@ -4,63 +4,65 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class ForwardAlgorithm {
+
 	private double[] stateProbability;
 	private double[][] transitionMatrix;
-	private HashMap<Point, Integer> mapping;
-	private Point[] mapping2;
+	private HashMap<State, Integer> mapping;
+	private State[] mapping2;
 
 	public ForwardAlgorithm(int size) {
-		int matrixSize = size * size;
+		int matrixSize = 4 * size * size;
 		stateProbability = new double[matrixSize];
 		transitionMatrix = new double[matrixSize][matrixSize];
-		
-		mapping = new HashMap<Point, Integer>();
-		mapping2 = new Point[size];
-		
-		for (int i = 0; i < matrixSize; i++) {
-			Point point = new Point(i % size, i / size); 
-			mapping.put(point, i);
-			mapping2[i] = point;
-			stateProbability[i] = 1.0/matrixSize;
+
+		mapping = new HashMap<State, Integer>();
+		mapping2 = new State[size];
+
+		for (int i = 0; i < size * size; i++) {
+			for (int j = 0; j < 4; j++) {
+				State state = null;
+				switch (j) {
+				case State.N:
+					state = new State(new Point(i / size, i % size), State.N);
+					break;
+				case State.S:
+					state = new State(new Point(i / size, i % size), State.S);
+					break;
+				case State.E:
+					state = new State(new Point(i / size, i % size), State.E);
+					break;
+				case State.W:
+					state = new State(new Point(i / size, i % size), State.W);
+					break;
+				default:
+					System.out.println("something wrong");
+				}
+				mapping.put(state, i);
+				mapping2[i] = state;
+				stateProbability[i] = 1.0 / matrixSize;
+			}
 		}
-		
+
 		for (int i = 0; i < matrixSize; i++) {
-			Point from = mapping2[i];
-			HashSet<Point> neighbours = new HashSet<Point>();
-			neighbours = calcNeighbours(from, size);
+			State from = mapping2[i];
 			for (int j = 0; j < matrixSize; j++) {
-				if (i == j) {
-					transitionMatrix[i][j] = 0;
-				} else if (neighbours.contains(mapping2[j])) {
-					transitionMatrix[i][j] = 1.0 / neighbours.size();
+				State to = mapping2[j];
+				if (from.isNeighbour(to)) {
+					if (from.encounterWall(size)) {
+						transitionMatrix[i][j] = 1.0 / from
+								.amountOfNeighbours(size);
+					} else if (from.heading == to.heading) {
+						transitionMatrix[i][j] = 0.7;
+					} else {
+						transitionMatrix[i][j] = 0.3 / (from
+								.amountOfNeighbours(size) - 1);
+					}
 				} else {
 					transitionMatrix[i][j] = 0;
 				}
-
 			}
 		}
-		
 	}
 
-	private HashSet<Point> calcNeighbours(Point from, int size) {
-		HashSet<Point> neighbours = new HashSet<Point>();
-		if (from.x <= 0) {
-			neighbours.add(new Point(from.x + 1, from.y));
-		} else if (from.x >= size - 1) {
-			neighbours.add(new Point(from.x - 1, from.y));
-		} else {
-			neighbours.add(new Point(from.x - 1, from.y));
-			neighbours.add(new Point(from.x + 1, from.y));
-		}
-		if (from.y <= 0) {
-			neighbours.add(new Point(from.x, from.y + 1));
-		} else if (from.y >= size - 1) {
-			neighbours.add(new Point(from.x, from.y - 1));
-		} else {
-			neighbours.add(new Point(from.x, from.y - 1));
-			neighbours.add(new Point(from.x, from.y + 1));
-		}
 
-		return neighbours;
-	}
 }
--- eda132/src/probabilistic_reasoning/State.java
+++ eda132/src/probabilistic_reasoning/State.java
@@ -1,24 +1,62 @@
 package probabilistic_reasoning;
 
 public class State {
+	public static final int N = 0;
+	public static final int E = 1;
+	public static final int S = 2;
+	public static final int W = 3;
+
 	Point p;
 	int heading;
-	
-	public State(Point p,int heading){
+
+	public State(Point p, int heading) {
 		this.p = p;
 		this.heading = heading;
 	}
-	
-	public boolean equals(Object obj){
-		State s = (State)obj;
+
+	public boolean equals(Object obj) {
+		State s = (State) obj;
 		return (s.p.equals(p) && s.heading == heading);
 	}
-	
+
 	@Override
 	public int hashCode() {
-		   int hash = 7;
-		    hash = 71 * hash + p.hashCode();
-		    hash = 71 * hash + heading;
-		    return hash;
+		int hash = 7;
+		hash = 71 * hash + p.hashCode();
+		hash = 71 * hash + heading;
+		return hash;
+	}
+
+	public boolean isNeighbour(State state) {
+		Point point = state.p;
+		if (point.x == p.x + 1 && point.y == p.y && state.heading == State.N
+				|| point.x == p.x - 1 && point.y == p.y
+				&& state.heading == State.S || point.x == p.x
+				&& point.y == p.y + 1 && state.heading == State.E
+				|| point.x == p.x && point.y == p.y - 1
+				&& state.heading == State.W) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int amountOfNeighbours(int size){
+		int amount = 4;
+		if(p.x<= 0 || p.x>= size-1){
+			amount--;
+		}
+		if(p.y<= 0 || p.y>= size-1){
+			amount--;
+		}
+		return amount;
+	}
+
+	public boolean encounterWall(int size) {
+		if (heading == State.N && p.y <= 0 || heading == State.S
+				&& p.y >= size - 1 || heading == State.W && p.x <= 0
+				|| heading == State.E && p.x >= size - 1) {
+			return true;
+		}
+		return false;
 	}
 }

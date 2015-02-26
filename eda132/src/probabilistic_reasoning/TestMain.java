941868102b4d9d7467963aa9a3ff4cc90b701ff2
dat11fp1 <freedrikp@users.noreply.github.com>
2015-02-26 11:04:34 +0100

    TestMain

--- eda132/src/probabilistic_reasoning/TestMain.java
+++ eda132/src/probabilistic_reasoning/TestMain.java
@@ -6,10 +6,15 @@ public class TestMain {
 	public static void main(String[] args) {
 		MovingBot mb = new MovingBot(8);
 		MovingBotGraphics graphics = new MovingBotGraphics(8);
+		ForwardAlgorithm fa = new ForwardAlgorithm(8);
 		while(true){
 			Point sensorPos = mb.sensorOutput();
 			Point botPos = mb.pos();
 			graphics.updateView(botPos, sensorPos);
+			Point estimatePos = fa.getEstimatedLocation(sensorPos);
+			System.out.println("Bot: " + botPos);
+			System.out.println("Sensor: " + botPos);
+			System.out.println("Estimate: " + botPos);
 			mb.move();
 			try {
 				Thread.sleep(1000);

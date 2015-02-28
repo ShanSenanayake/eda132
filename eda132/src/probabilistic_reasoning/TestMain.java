6cb7bfd19c81af4fba851ae0aa5786240bad0ab0
ShanSenanayake <senanayake92@gmail.com>
2015-02-28 11:36:49 +0100

    agkd börjat skriva litew

--- eda132/src/probabilistic_reasoning/TestMain.java
+++ eda132/src/probabilistic_reasoning/TestMain.java
@@ -1,38 +1,44 @@
 package probabilistic_reasoning;
 
-
 public class TestMain {
 
 	public static void main(String[] args) {
 		MovingBot mb = new MovingBot(8);
-		MovingBotGraphics graphics = new MovingBotGraphics(8);
-		ForwardAlgorithm fa = new ForwardAlgorithm(8);
-		int iterations = 0;
-		int estimates = 0;
-		while(true){
-			iterations++;
-			Point sensorPos = mb.sensorOutput();
-			Point botPos = mb.pos();
-			Point estimatePos = fa.getEstimatedLocation(sensorPos);
-			if(botPos.equals(estimatePos)){
-				estimates++;
-			}
-			double prob = ((double)(estimates))/iterations;
-			System.out.println("Probability: " + prob*100 + "% " + estimates + " " + iterations + " dist: " + botPos.distance(estimatePos));
-			graphics.updateView(botPos, sensorPos,estimatePos);
-//			System.out.println("Bot: " + botPos);
-//			System.out.println("Sensor: " + sensorPos);
-//			System.out.println("Estimate: " + estimatePos);
-			mb.move();
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+//		MovingBotGraphics graphics = new MovingBotGraphics(8);
+		double[] error = new double[1000];
+		double[] probability = new double[1000];
+		for (int i = 0; i < 100; i++) {
+			ForwardAlgorithm fa = new ForwardAlgorithm(8);
+			int estimates = 0;
+			int iterations = 0;
+			while (iterations < 1000) {
+				iterations++;
+				Point sensorPos = mb.sensorOutput();
+				Point botPos = mb.pos();
+				Point estimatePos = fa.getEstimatedLocation(sensorPos);
+				if (botPos.equals(estimatePos)) {
+					estimates++;
+				}
+				double prob = ((double) (estimates)) / iterations;
+				// System.out.println("Probability: " + prob*100 + "% " +
+				// estimates + " " + iterations + " dist: " +
+				// botPos.distance(estimatePos));
+				// graphics.updateView(botPos, sensorPos,estimatePos);
+				// System.out.println("Bot: " + botPos);
+				// System.out.println("Sensor: " + sensorPos);
+				// System.out.println("Estimate: " + estimatePos);
+				error[iterations - 1] += botPos.distance(estimatePos);
+				probability[iterations - 1] += prob;
+				mb.move();
 			}
 		}
-		
-
-
+		for (int i = 0; i < 1000; i++) {
+			System.out.println(error[i] / 100);
+		}
+		System.out.println("--------------");
+		for (int i = 1; i <= 1000; i++) {
+			System.out.println(i);
+		}
 	}
 
 }

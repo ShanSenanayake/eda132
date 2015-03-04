1bc06e073c0702cfbd5cc86577aeb6dfe939a4f9
ShanSenanayake <senanayake92@gmail.com>
2015-03-04 15:49:53 +0100

    almost done with basic!

--- eda132/src/decision_trees/DecisionTreeAlgorithm.java
+++ eda132/src/decision_trees/DecisionTreeAlgorithm.java
@@ -3,36 +3,41 @@ package decision_trees;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class DecisionTreeAlgorithm {
 
 	public DecisionTreeAlgorithm() {
 	}
-	
-	public DecisionNode decisionTreeLearning(ArrayList<Example> examples, ArrayList<Attribute> attributes, ArrayList<Example> parentExamples){
-		if(examples.isEmpty()){
+
+	public DecisionNode decisionTreeLearning(ArrayList<Example> examples,
+			ArrayList<Attribute> attributes, ArrayList<Example> parentExamples) {
+		if (examples.isEmpty()) {
 			return pluralityValue(parentExamples);
-		}else if(hasSameGoals(examples)){
+		} else if (hasSameGoals(examples)) {
 			return new TerminalNode(examples.get(0).getGoal());
-		}else if(attributes.isEmpty()){
+		} else if (attributes.isEmpty()) {
 			return pluralityValue(examples);
-		}else{
-			Attribute attr = mostImporatant(attributes,examples);
-			AttributeNode root =  new AttributeNode(attr);
-			for(String value: attr){
+		} else {
+			Attribute attr = mostImporatant(attributes, examples);
+			AttributeNode root = new AttributeNode(attr);
+			for (String value : attr) {
 				ArrayList<Example> rootExamples = new ArrayList<Example>();
-				for(Example ex: examples){
-					if(value == ex.getValue(attr)){
+				for (Example ex : examples) {
+					if (value == ex.getValue(attr)) {
 						rootExamples.add(ex);
 					}
 				}
 				ArrayList<Attribute> newAttributes = new ArrayList<Attribute>();
-				for(Attribute a: attributes){
-					if(!a.equals(attr)){
+				for (Attribute a : attributes) {
+					if (!a.equals(attr)) {
 						newAttributes.add(a);
 					}
 				}
-				root.addBranch(value,decisionTreeLearning(rootExamples,newAttributes,examples));
+				root.addBranch(
+						value,
+						decisionTreeLearning(rootExamples, newAttributes,
+								examples));
 			}
 			return root;
 		}
@@ -41,37 +46,37 @@ public class DecisionTreeAlgorithm {
 	private Attribute mostImporatant(ArrayList<Attribute> attributes,
 			ArrayList<Example> examples) {
 		// TODO Auto-generated method stub
-		return attributes.get((int)(Math.random()*attributes.size()));
+		return attributes.get((int) (Math.random() * attributes.size()));
 	}
 
 	private boolean hasSameGoals(ArrayList<Example> examples) {
 		Goal g = examples.get(0).getGoal();
-		for (Example ex: examples){
-			if (!g.equals(ex.getGoal())){
+		for (Example ex : examples) {
+			if (!g.equals(ex.getGoal())) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	private DecisionNode pluralityValue(ArrayList<Example> parentExamples) {
-		HashMap<Example,Integer> map = new HashMap<Example,Integer>();
-		for (Example ex: parentExamples){
+	private DecisionNode pluralityValue(ArrayList<Example> examples) {
+		HashMap<Goal, Integer> map = new HashMap<Goal, Integer>();
+		for (Example ex : examples) {
 			int i = 1;
-			if (map.containsKey(ex)){
-				i = map.get(ex);
+			if (map.containsKey(ex.getGoal())) {
+				i = map.get(ex.getGoal());
 				i++;
 			}
-			map.put(ex, i);
+			map.put(ex.getGoal(), i);
 		}
 		int occs = 0;
-		Example ex = null;
-		for (Map.Entry<Example, Integer> entry:map.entrySet()){
-			if (entry.getValue() > occs){
+		Goal g = null;
+		for (Entry<Goal, Integer> entry : map.entrySet()) {
+			if (entry.getValue() > occs) {
 				occs = entry.getValue();
-				ex = entry.getKey();
+				g = entry.getKey();
 			}
 		}
-		return ex;
+		return new TerminalNode(g);
 	}
 }

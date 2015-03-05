faa22629a33bd76345dfe0a66ad3945beab63d2c
freedrikp <fredrik40@msn.com>
2015-03-05 11:16:09 +0100

    Integral values working

--- eda132/src/decision_trees/Test.java
+++ eda132/src/decision_trees/Test.java
@@ -5,8 +5,14 @@ import java.io.File;
 public class Test {
 
 	public static void main(String[] args) {
-		DecisionTreeParser dtp = new DecisionTreeParser(new File("arff18-3.txt"),"yes");
+//		DecisionTreeParser dtp = new DecisionTreeParser(new File("arff18-3.txt"),"yes");
+//		DecisionTreeParser dtp = new DecisionTreeParser(new File("diabetes.arff"),"tested_positive");
+		DecisionTreeParser dtp = new DecisionTreeParser(new File("weather.nominal.arff"),"yes");
 		Relation rel = dtp.getRelation();
+//		for (Attribute a : rel.getAttributes()){
+//			a.setSplitPoint(5);
+//		}
+		rel.calculateGain();
 		DecisionTreeAlgorithm dta = new DecisionTreeAlgorithm(rel);
 		System.out.println(dta.dtl().print(0));
 	}
